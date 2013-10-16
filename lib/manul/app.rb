07eae1eaa10ccd9c59c1f56df970d4519be9f0f1
Konstantin Kosmatov <key@kosmatov.ru>
2013-10-16 08:08:28 +0000

    File serving application added

---
+++ lib/manul/app.rb
@@ -0,0 +1,37 @@
+module Manul
+  class App
+    def initialize(options)
+      @path = options[:path]
+    end
+
+    def call(env)
+      process env
+      [@status, @headers, @content]
+    end
+
+    def process(env)
+      filepath = File.join @path, env[:filepath]
+      filepath = File.realpath filepath
+
+      if filepath[@path]
+        @content = File.open filepath
+        @status = 200
+      else
+        @content = "403 Forbidden\n"
+        @status = 403
+      end
+    rescue Errno::ENOENT => e
+      @content = "404 Not Found\n"
+      @status = 404
+    rescue Exception => e
+      @content = "400 Bad Request\n"
+      @status = 400
+    ensure
+      length = @content.is_a?(String) ? @content.length : @content.stat.size
+      @headers = [
+        "Content-Length: #{length}"
+      ]
+    end
+
+  end
+end
