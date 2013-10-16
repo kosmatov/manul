5f78b13f460895ed8d69374a53c7129de28d2aa3
Konstantin Kosmatov <key@kosmatov.ru>
2013-10-16 09:06:09 +0000

    Request parsing improved

--- lib/manul/request.rb
+++ lib/manul/request.rb
@@ -7,9 +7,15 @@ module Manul
     end
 
     def parse(data)
-      head = data.split("\r\n").first
-      p head
-      @env[:filepath] = URI::decode head.split[1].to_s
+      head = data.split("\r\n").first.split
+
+      @env = {
+        method: head[0],
+        filepath: URI::decode(head[1].to_s),
+        proto: head[2]
+      }
+
+      p head.join ' '
     end
   end
 end
